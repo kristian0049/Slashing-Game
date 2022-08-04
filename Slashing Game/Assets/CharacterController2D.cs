b83dc7037944c417a2b0f672c8e0863a6cf8dae2
kriskobeast@gmail.com <kriskobeast@gmail.com>
2022-08-04 20:26:34 +0100

    Deleted Project Files, I will add new ones

--- Slashing Game/Assets/CharacterController2D.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9aa25de5a817f5650ba1d66ad5829c7e35c5ee725138d5b9c1c263ec348e73b2
-size 1147
--- Slashing Game/Assets/PlayerMovement.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f12057afb9c190b67400887c50920442d9d241f3a87c7dd54fd0e45071b2a9b0
-size 496
