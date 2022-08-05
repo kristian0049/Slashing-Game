c0ce675abbedef663cb8b25588fba7dccd8d91cd
kriskobeast@gmail.com <kriskobeast@gmail.com>
2022-08-05 13:57:01 +0100

    Added Some Movement , I need to figure out how to make the character slows downs when the movement key is released like he is sliding on ice, but i might scrap that idead

--- Slash-Game/Assets/Scenes/CharacterController2D.cs
+++ Slash-Game/Assets/Scenes/CharacterController2D.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:860c639cc354fb20c2729726de60679555dfb801440a6749f2e4a61cda35814e
-size 1051
+oid sha256:1c90df74b1bbbfb860c43cebcc640828db87baee1c1bb729fe8deb87318f1a3c
+size 2492
